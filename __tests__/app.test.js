ea37f38c3eea57d42124610a51ea9e7590391459
billymathis <142643264+billymathis@users.noreply.github.com>
2024-05-04 11:45:51 +0530

    Update app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../app');
 
 describe("GET /", () => {
-  test("It should respond with 'Hello World'", async () => {
+  test("It should respond with 'Hello World Deployed through github actions!!!'", async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe('Hello World');
