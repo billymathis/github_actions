548b79be46bb1932b417cbd872134c18e2e2f574
billymathis <142643264+billymathis@users.noreply.github.com>
2024-05-04 13:14:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World Deployed through Github Actions!!!');
+  res.send('Latest');
 });
 
 module.exports = app;  
