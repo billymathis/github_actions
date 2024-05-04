4052a1c08d55f6c063029486364a2d6f847548a4
billymathis <142643264+billymathis@users.noreply.github.com>
2024-05-04 13:56:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Latest funny');
+  res.send('Latest funny man');
 });
 
 module.exports = app;  
